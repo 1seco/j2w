130146fb644970fa8cf64c2d325a4b79153c1a75
1994-MR2-SW20 <71955406+1seco@users.noreply.github.com>
2024-06-30 15:21:09 +0800

    random include i dont remember nor need removed

--- conv.c
+++ conv.c
@@ -3,11 +3,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <threads.h>
 #include <time.h>
 
 time_t current = {0};
-Date current_local = {0};
+struct tm* current_local = {0};
 
 regex_t regex;
 const char* format = "^([0-9]{2}|%)/[0-9]{2}/[0-9]{4}-([0-9]{2}|%)[:][0-9]{2}$";
@@ -23,13 +22,13 @@ int verifyRepeatString(const char* src) {
 
 void initTime(void) {
   current = time(NULL);
-  current_local.time = localtime(&current);
+  current_local = localtime(&current);
   compileRegex();
 }
 
 void deInitAll() {
-  free(current_local.time);
-  current_local.time = NULL;
+  free(current_local);
+  current_local = NULL;
   regfree(&regex);
 }
 
@@ -38,38 +37,38 @@ bool isLeap(int year) {
 }
 
 void attemptSetDate(Date* date, char** split_str) {
-  date->time->tm_mday = atoi(split_str[0]);
-  date->time->tm_mon = atoi(split_str[1]);
-  date->time->tm_year = atoi(split_str[2]);
-  date->time->tm_hour = atoi(split_str[3]);
-  date->time->tm_min = atoi(split_str[4]);
+  date->time.tm_mday = atoi(split_str[0]);
+  date->time.tm_mon = atoi(split_str[1]);
+  date->time.tm_year = atoi(split_str[2]);
+  date->time.tm_hour = atoi(split_str[3]);
+  date->time.tm_min = atoi(split_str[4]);
   for (short i = 0; i < 5; i++) {
     date->is_repeat[i] = *split_str[i] == '%' ? true : false;
   }
 }
 
 bool verifyDate(Date in) {
-  if (in.time->tm_hour > 23 || in.time->tm_hour < 0)
+  if (in.time.tm_hour > 23 || in.time.tm_hour < 0)
     return false;
-  if (in.time->tm_min > 59 || in.time->tm_min < 0)
+  if (in.time.tm_min > 59 || in.time.tm_min < 0)
     return false;
-  if (in.time->tm_year > current_local.time->tm_year + 500 ||
-      in.time->tm_year < current_local.time->tm_year - 500) {
+  if (in.time.tm_year > current_local->tm_year + 500 ||
+      in.time.tm_year < current_local->tm_year - 500) {
     return false;
   }
-  if (in.time->tm_mon < 0 || in.time->tm_mon > 12)
+  if (in.time.tm_mon < 0 || in.time.tm_mon > 12)
     return false;
-  if (in.time->tm_mday < 0 || in.time->tm_mday > 31)
+  if (in.time.tm_mday < 0 || in.time.tm_mday > 31)
     return false;
-  if (in.time->tm_mon == 2) {
-    if (isLeap(in.time->tm_year + 1900))
-      return (in.time->tm_mday <= 29);
+  if (in.time.tm_mon == 2) {
+    if (isLeap(in.time.tm_year + 1900))
+      return (in.time.tm_mday <= 29);
     else
-      return (in.time->tm_mday <= 28);
+      return (in.time.tm_mday <= 28);
   }
-  if (in.time->tm_mon == 4 || in.time->tm_mon == 6 || in.time->tm_mon == 9 ||
-      in.time->tm_mon == 11)
-    return (in.time->tm_mday <= 30);
+  if (in.time.tm_mon == 4 || in.time.tm_mon == 6 || in.time.tm_mon == 9 ||
+      in.time.tm_mon == 11)
+    return (in.time.tm_mday <= 30);
   return true;
 }
 
@@ -81,6 +80,7 @@ Date convMain(char* src, char* format) {
   for (; len <= 5; len++) {
     if (split_strings[len] != NULL)
       len++;
+    else break;
   }
   if (len < 5)
     return temp;
